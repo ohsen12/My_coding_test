c4e8b0d1bb0db1fc60be2b29a1c292c599ed0e62
ohsen <tpsdms12@naver.com>
2024-12-11 16:25:14 +0900

    Update 주사위 게임 3.py

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -4,13 +4,13 @@ def solution(a, b, c, d):
     if max(counts) == 4:
         return a * 1111
     elif max(counts) == 3:
-        p = nums[counts.index(3)]
-        q = nums[counts.index(1)]
+        p = nums[counts.index(3)] # p는 세 번 나온 숫자
+        q = nums[counts.index(1)] # q는 한 번 나온 숫자
         return (10 * p + q) ** 2
     elif max(counts) == 2:
-        if min(counts) == 2:
+        if min(counts) == 2: # 두 쌍의 값이 같은 경우
             return (a + c) * abs(a - c) if a == b else (a + b) * abs(a - b)
-        else:
+        else: # 한 쌍의 값이 같고 나머지 두 값이 다를 때
             p = nums[counts.index(2)]
             return (a * b * c * d) / p**2
     else:
