9711701666083c5a873bf38ece9880ce00bb616b
ohsen <tpsdms12@naver.com>
2024-12-12 10:26:49 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,2 +1,3 @@
+# not in 연산자는 특정 값이 시퀀스(리스트, 문자열, 튜플 등)에 포함되지 않는지 확인할 때 사용
 def solution(my_string):
-    return ''.join(i for i in my_string if i not in ['a','e','i','o','u'])
+    return ''.join(i for i in my_string if i not in 'aeiou')
