19f2a9c0d0b80f7ee324ec5048162b194413d6dd
ohsen <tpsdms12@naver.com>
2024-12-12 16:00:31 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,20 +1,2 @@
-def solution(my_string, m, c):
-    temp=[]
-    temp2= list(my_string)
-    idx=0 
-
-    for i in temp2: 
-        idx += 1
-        if m == c :
-            if idx % m == 0 : # 마지막 열을 읽는 경우.
-                temp += i 
-
-        elif idx % m == c : # 1~ m-1열 중 하나를 읽는 경우.
-            temp += i 
-
-
-
-    temp = ''.join(map(str,temp))  
-    answer = temp
-
-    return answer
+def solution(s, m, c):
+    return s[c-1::m]
