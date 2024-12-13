884b635e50313087610216949355c67caa446424
ohsen <tpsdms12@naver.com>
2024-12-13 09:53:05 +0900

    [level 0] Title: 수 조작하기 1, Time: 9.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,11 +1,5 @@
 def solution(n, control):
+    c = {'w':1, 's':-1, 'd':10, 'a':-10}
     for i in control:
-        if i == 'w':
-            n+=1
-        elif i == 's':
-            n-=1
-        elif i == 'd':
-            n+=10
-        else:
-            n-=10
+        n += c[i]
     return n
