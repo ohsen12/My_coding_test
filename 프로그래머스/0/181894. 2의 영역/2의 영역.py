1874a848563e83757ced8c5111b19a925751ae48
ohsen <tpsdms12@naver.com>
2024-12-15 22:06:27 +0900

    [level 0] Title: 2의 영역, Time: 2.11 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,7 +1,6 @@
 def solution(arr):
-    try:
-        start = arr.index(2)  # 첫 번째 2의 인덱스 찾기
-        end = len(arr) - 1 - arr[::-1].index(2)  # 마지막 2의 인덱스 찾기
-        return arr[start:end + 1]
-    except: #2가 없으면 에러가 발생하므로 [-1] 반환
+    if 2 not in arr:
         return [-1]
+    start = arr.index(2)      # 2가 처음으로 나오는 인덱스
+    end = len(arr) - arr[::-1].index(2)  # 2가 마지막으로 나오는 인덱스 계산
+    return arr[start:end] 
