f40e0d7a042d008866f65c3d30a9ca100ab2c9f1
ohsen <tpsdms12@naver.com>
2024-12-15 15:43:13 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -1,2 +1,5 @@
-def solution(start_num, end_num):
-    return list(reversed([i for i in range(end_num,start_num+1)]))
+# range 함수에 세 번째 인자로 -1을 주면 역순의 숫자 시퀀스를 생성할 수 있다.
+# 이 경우 첫 번째 인자가 두 번째 인자보다 커야지 음수 스텝 값(-1)과 함께 사용할 수 있다.
+
+def solution(start, end):
+    return list(range(start,end-1,-1))
