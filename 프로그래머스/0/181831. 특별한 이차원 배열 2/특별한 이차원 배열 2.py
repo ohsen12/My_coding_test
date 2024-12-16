989e70540f4cbaa657d1b93b0d1dc3fe859a669f
ohsen <tpsdms12@naver.com>
2024-12-16 14:07:00 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.90 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -2,6 +2,6 @@ def solution(arr):
     n = len(arr)
     for i in range(n): # 배열의 크기만큼 범주를 정해준다.
         for j in range(n):
-            if arr[i][j] != arr[j][i]: # 배전체 배열 검토
+            if arr[i][j] != arr[j][i]: # 전체 배열 검토
                 return 0
     return 1
