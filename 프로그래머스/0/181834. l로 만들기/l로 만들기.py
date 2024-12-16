e48d7ecb4f121510029712bdfe1429fa30005a98
ohsen <tpsdms12@naver.com>
2024-12-16 10:54:20 +0900

    [level 0] Title: l로 만들기, Time: 9.95 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,8 +1,6 @@
-def solution(my):
-    str = 'abcdefghijk'
-    my = list(my)
-    for i in range(len(my)):
-        if my[i] in str:
-            my[i] = 'l'
-    return ''.join(my)
-            
+# 파이썬에서는 문자열 비교 시 유니코드 코드 포인트를 사용하여 사전순으로 비교한다.
+# 유니코드는 각 문자에 고유한 숫자 값을 부여하여 일관된 문자 표현을 가능하게 한다.
+# 각 알파벳의 유니코드 코드 포인트는 ord() 함수를 통해 확인할 수 있으며 사전 순으로 커진다.
+
+def solution(myString):
+    return ''.join(x if x > 'l' else 'l' for x in myString)
