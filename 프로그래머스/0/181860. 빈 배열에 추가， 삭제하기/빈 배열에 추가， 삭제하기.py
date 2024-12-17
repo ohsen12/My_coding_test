2fbcc91d3dae543c80e4796bbe68bb26a3011bba
ohsen <tpsdms12@naver.com>
2024-12-17 10:48:20 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -1,10 +1,12 @@
+# 두 이터러블의 길이가 다를 때 zip 함수는 짧은 이터러블의 길이에 맞춰 작동한다. 즉, 짧은 이터러블의 끝에서 반복을 멈춘다.
 # n개의 원소를 제거하려면 슬라이싱을 [:-n]까지 해주면 됨. 만약 [:-2]라면 끝에서 두 개는 포함하지 않고 자르겠다는 것.
+
 
 def solution(arr, flag):
     x = []
-    for i in range(len(arr)):
-        if flag[i]:
-            x.extend([arr[i]]*arr[i]*2)
+    for i,j in zip(arr,flag):
+        if j:
+            x.extend([i]*i*2)
         else:
-            x = x[:-arr[i]]
+            x = x[:-i]
     return x
