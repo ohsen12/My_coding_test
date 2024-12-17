91a7ba32a03af3e54f6aa496ada5b9326e14eb72
ohsen <tpsdms12@naver.com>
2024-12-17 09:54:10 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+def solution(num_list):
+    l = []
+    for i in num_list:
+        count = 0
+        while True:
+            if i==1:
+                break
+            elif i%2:
+                i = (i-1)/2
+            else:
+                i = i/2
+            count += 1
+        l.append(count)
+    return sum(l)
