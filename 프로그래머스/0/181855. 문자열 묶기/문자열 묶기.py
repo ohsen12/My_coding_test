f63d4fa5a17a9da3c3d640b8be6a6737cba3111c
ohsen <tpsdms12@naver.com>
2024-12-19 19:00:15 +0900

    [level 0] Title: 문자열 묶기, Time: 25.11 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -5,5 +5,17 @@ def solution(strarr):
     for i in strarr:
         my_dict[len(i)] +=1 # 해당 길이 키의 값에 +=1 해주기
     return max(my_dict.values()) # 가장 큰 값 구하기
+
+'''
+# 메모리 사용이 덜 한 풀이
+def solution(strArr):
+    m = {}
+    for s in strArr:
+        if len(s) in m:     # 해당 길이의 키가 이미 있다면
+            m[len(s)] += 1  # 그 키의 값을 +=1 해주기
+        else:
+            m[len(s)] = 1   # 새로운 길이가 있을 때마다 해당길이 키를 생성해주기
+    return max(m.values())
+'''
         
         
