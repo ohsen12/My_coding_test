59ddc9fdeb35ea2a5589307fc72d179f9f48c534
ohsen <tpsdms12@naver.com>
2024-12-20 09:29:30 +0900

    [level 0] Title: 배열 만들기 6, Time: 117.21 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/README.md
+++ 프로그래머스/0/181859. 배열 만들기 6/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 6 - 181859 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859?language=python3) 
 
 ### 성능 요약
 
-메모리: 18.1 MB, 시간: 216.11 ms
+메모리: 18 MB, 시간: 117.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 20일 09:20:02
+2024년 12월 20일 09:29:28
 
 ### 문제 설명
 
--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -1,14 +1,12 @@
+# 논리 연산자 or는 조건식으로 사용할 때와 표현식으로 사용할 때 약간 다르게 동작한다.
+# 조건식으로 사용될 때 or 연산자는 첫번째가 참이면 두번째는 평가하지 않고 True를 반환한다.
+# or 연산자가 표현식으로 사용될 때는 첫번째 값이 참인 경우 첫번째를 반환하고 두번째 값은 아예 평가되지 않는다.
+
 def solution(arr):
-    stk=[]
-    i=0
-    while i < len(arr):
-        if not(stk):
-            stk.append(arr[i])
-            i+=1
-        elif stk and stk[-1]==arr[i]:
+    stk = []
+    for i in range(len(arr)):
+        if stk and stk[-1] == arr[i]:
             stk.pop()
-            i+=1
-        elif stk and stk!=arr[i]:
+        else:
             stk.append(arr[i])
-            i+=1
-    return stk if stk else [-1]
+    return stk or [-1]   #stk가 빈배열이 아니라면 True라서 stk 반환되고 후건은 평가되지 않고 끝난다.
